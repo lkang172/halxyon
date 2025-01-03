846213ce2b517413134285a6ae7fd22d40f39d78
Lucas Kang <kangzundao@gmail.com>
2025-01-02 22:58:02 -0500

    implemented auth status checker + name displayed after signin

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,14 +1,44 @@
 import Navbar from "./Navbar";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../Backend/Firebase/firebaseConfig.ts";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  getAuth,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
+import app, { auth } from "../Backend/Firebase/firebaseConfig.ts";
 
 const Profile = () => {
+  const [user, setUser] = useState<User | null>(null);
   const handleGoogle = async () => {
     const provider = await new GoogleAuthProvider();
-    return signInWithPopup(auth, provider);
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Error signing in with Google: ", error);
+    }
   };
-  <h1>Happy new year!</h1>;
-  return <button onClick={handleGoogle}>Sign in</button>;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        console.log("Welcome, " + user.displayName);
+      } else {
+        console.log("No user is signed in");
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Happy new year!</h1>
+      <button onClick={handleGoogle}>Sign in</button>
+      <p>Welcome, {user?.displayName}</p>
+    </>
+  );
 };
 
 export default Profile;
